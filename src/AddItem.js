3c7bd261b9a6a8b2866052251e2f0673a6fc39bd
gaganMadan <gagan7535@gmail.com>
2020-09-22 12:04:36 +0530

    2 files modified

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
  
 const AddItem = () => {
-  //const [name, setName] = useState('');
+  const [name, setName] = useState('');
+  const [skill, setSkill] = useState(['']);
 
+  const submitHandler = e => {
+    e.preventDefault();
+    alert(`Hello ${name} Skills ${skill}`)
+  }
 
   return (
-    <form onSubmit={submitValues
-    }>
-      <span> Please Enter Name : </span>
-      <input type="text" value={name} onChange={handler}/>
+    <form onSubmit={submitHandler}>
+      <div>
+        <label> Please Enter Name : </label>
+        <input type="text" value={name} onChange={e => setName(e.target.value)}/>
+      </div>
+      <div className="skillSet">
+         <label> Enter Skill Set : </label>
+        <p>
+         <input type="text" value={skill[0]} onChange={e => setSkill(...skill, e.target.value)}/>
+        </p>
+        <p>
+          <input type="text" value={skill[1]} onChange={e => setSkill(...skill, e.target.value)}/>
+        </p>
+        <p>
+          <input type="text" value={skill[2]} onChange={e => setSkill(...skill, e.target.value)}/>
+        </p>
+      </div>
       <input type="submit"/>
     </form>
   )
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 import Listing from './Listing';
 import Nav from './Nav';
+import AddItem from  './AddItem'
 import {ItemsProvider} from './ItemsContext';
 
 const App = () => (
@@ -9,6 +10,7 @@ const App = () => (
       <div className="App">
           <Nav />
           <Listing />
+          <AddItem />
       </div>
     </ItemsProvider>
     
